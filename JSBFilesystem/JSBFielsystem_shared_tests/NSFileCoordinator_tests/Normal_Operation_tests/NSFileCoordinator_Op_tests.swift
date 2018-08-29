ba4a9fe8984e5ddf5d4e2801761a3dadff30bb7f
Jeffrey Bergier <jeffburg@jeffburg.com>
2018-08-29 12:30:23 -0400

    added file for some more tests

---
+++ JSBFilesystem/JSBFielsystem_shared_tests/NSFileCoordinator_tests/Normal_Operation_tests/NSFileCoordinator_Op_tests.swift
@@ -0,0 +1,13 @@
+//
+//  NSFileCoordinator_Op_tests.swift
+//  JSBFilesystem
+//
+//  Created by Jeffrey Bergier on 8/29/18.
+//
+
+import XCTest
+import JSBFilesystem
+
+class NSFileCoordinator_Op_tests: XCTestCase {
+    
+}
